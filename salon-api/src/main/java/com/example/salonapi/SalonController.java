b1c853192aa191b2a083f1510fb6c25ffb8657ca
JimmyVila <jimmy.leiva@comsysto.com>
2021-06-23 12:31:44 +0200

    update milenstone 2

--- salon-api/src/main/java/com/example/salonapi/SalonController.java
+++ salon-api/src/main/java/com/example/salonapi/SalonController.java
@@ -5,16 +5,19 @@ import com.example.salonapi.SalonServiceDetail.SalonRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping
+
 public class SalonController {
 
-
-    @GetMapping("/")
-    public String intro() {
+    @GetMapping("/api/services/retrieveAvailableSalonServices")
+    public String t() {
         return "Intro page";
     }
+
 
 
     @GetMapping("/salon")
--- salon-ui/src/App.js
+++ salon-ui/src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Test from "./Components/Test";
 
 function App() {
   return (
     <div >
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a className="navbar-brand" style={{"padding-left":"20px"}}>
                 AR Salon and Day Spa
-              </a>
+            </a>
         </nav>
+        <Test/>
     </div>
   );
 }
