31de4017a73f73c633dda18a5c1c40488848c400
JimmyVila <jimmy.leiva@comsysto.com>
2021-06-24 23:46:25 +0200

    milenstone 3 update ui

--- salon-ui/src/App.js
+++ salon-ui/src/App.js
@@ -1,22 +1,37 @@
 import './App.css';
 import LoadingIndicatorComponent from "./loader/LoadingIndicatorComponent";
 import AppNotificationComponent from "./Components/AppNotificationComponent";
-
 import ChooseService from "./Components/service/Choose-service";
+import {Route, Router} from "react-router";
+import Switch from "react-bootstrap/Switch";
+import ChooseSlot from "./Components/service/Choose-slot";
+import { createMemoryHistory } from 'history'
+
+const history = createMemoryHistory()
+
 function App() {
   return (
-    <div >
+    <Router history={history}>
         <LoadingIndicatorComponent/>
         <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
             <a className="navbar-brand" href="/">Ar Salon & Day Spa</a>
         </nav>
         <div role="main" className="container">
             <div className="padding-container">
-                <ChooseService>fsdf</ChooseService>
+                <Switch>
+                    <Route exact path="/" component={ChooseService}>
+                    </Route>
+                     <Route path="/chooseslot/:serviceId/:serviceName"  component={ChooseSlot}>
+                    </Route>
+                    <Route>
+                        <ChooseService />
+                    </Route>
+                </Switch>
+
             </div>
         </div>
         <AppNotificationComponent/>
-    </div>
+    </Router>
   );
 }
 
